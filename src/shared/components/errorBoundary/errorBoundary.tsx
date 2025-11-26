0d6ad7387616dc1324aa9d1a8748001ef6a038b0
Can Serinyel <cserinyel@gmail.com>
2025-11-26 16:19:13 +0300

    fix

--- src/shared/components/errorBoundary/errorBoundary.tsx
+++ src/shared/components/errorBoundary/errorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, ErrorInfo, ReactNode } from "react";
+import { Component, type ErrorInfo, type ReactNode } from "react";
 import ErrorOverlay from "../errorOverlay/errorOverlay";
 
 interface Props {
